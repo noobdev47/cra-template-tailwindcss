4ef83b9f63ac836458bccae6fa22033d5c79cd01
Hassan Ali <hassan@akhtarsitsolutions.com>
2021-04-14 14:43:17 +0500

    TailwindCSS, react-animations integrated

---
+++ template/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>React App with TailwindCSS</p>
+    </div>
+  )
+}
+
+export default App;
